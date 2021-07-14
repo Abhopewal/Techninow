651949afc1adf1b5ea5c09aa8ea1936c4a354495
Akash kumar <48316274+Abhopewal@users.noreply.github.com>
2021-07-14 19:01:26 +0530

    Delete Config.php

--- admin/Config.php
+++
@@ -1,9 +0,0 @@
-<!--Author Akash bhopewal-->
-<?php
-$hostpath="localhost";
-$host="localhost";
-$user="root";
-$password="";
-$conn = mysqli_connect($host, $user, $password) or die("Connetion not establish");
-mysqli_select_db($conn, "techninow");
-?>
